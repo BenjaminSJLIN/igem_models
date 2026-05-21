420b8dd2603d34a466544f7fa4264910d37308b7
wensontu0906-spec <wensontu0906@gmail.com>
2026-05-21 17:47:18 +0800

    Update equations.py

--- models/model_1_0/equations.py
+++ models/model_1_0/equations.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def hill_activation(ara, alpha_0, alpha_max, K_d, n):
+def K_D (cas12a-crRNA, target_DNA, cas12a):
     """
     Hill equation for promoter activation by Arabinose.
     P_act([Ara]) = alpha_0 + alpha_max * ([Ara]^n / (K_d^n + [Ara]^n))
     """
-    return alpha_0 + alpha_max * (ara**n / (K_d**n + ara**n))
+    return cas12a-crRNA*targetDNA/cas12a
 
 def cas12a_dynamics(y, t, ara, params):
     """
