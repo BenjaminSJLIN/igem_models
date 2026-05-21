290a633ecdcd31d549ce6c85a7711d42d9e7eb24
wensontu0906-spec <wensontu0906@gmail.com>
2026-05-21 18:03:55 +0800

    Update equations.py

--- models/model_1_0/equations.py
+++ models/model_1_0/equations.py
@@ -13,21 +13,4 @@ def cleavage_Rate(k_cat, cas12a_A, ssDNA, K_M):
     y[0] = mRNA
     y[1] = Cas12a protein
     """
-    mRNA, Cas12a = y
     
-    alpha_0 = params['alpha_0']
-    alpha_max = params['alpha_max']
-    K_d = params['K_d']
-    n = params['n']
-    gamma_m = params['gamma_m']
-    beta_p = params['beta_p']
-    gamma_p = params['gamma_p']
-    
-    # Promoter activation
-    P_act = hill_activation(ara, alpha_0, alpha_max, K_d, n)
-    
-    # Differential equations
-    dmRNA_dt = P_act - gamma_m * mRNA
-    dCas12a_dt = beta_p * mRNA - gamma_p * Cas12a
-    
-    return [dmRNA_dt, dCas12a_dt]
