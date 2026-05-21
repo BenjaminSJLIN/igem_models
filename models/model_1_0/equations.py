1b30943a197800394b0a1c6ac45ec479713e68b0
wensontu0906-spec <wensontu0906@gmail.com>
2026-05-21 18:03:31 +0800

    Update equations.py

--- models/model_1_0/equations.py
+++ models/model_1_0/equations.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def K_D (cas12a-crRNA, target_DNA, cas12a):
+def K_D (cas12a-crRNA, target_DNA, cas12a_A):
     """
     Hill equation for promoter activation by Arabinose.
     P_act([Ara]) = alpha_0 + alpha_max * ([Ara]^n / (K_d^n + [Ara]^n))
     """
     return cas12a-crRNA*targetDNA/cas12a
 
-def cas12a_dynamics(y, t, ara, params):
+def cleavage_Rate(k_cat, cas12a_A, ssDNA, K_M):
     """
     System of ODEs for Cas12a expression.
     y[0] = mRNA
